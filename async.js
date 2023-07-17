467960004cbe59fedb05c495593779874a7a941e
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-07-17 20:52:27 +0530

    asynch

---
+++ async.js
@@ -0,0 +1,56 @@
+// function fiveSec() {
+//   let ms = 5000 + new Date().getTime();
+//   while (new Date() < ms) {}
+// }
+
+
+
+// function greet() {
+//   fiveSec();
+//   console.log("hello world");
+// }
+
+// function welcom() {
+//   console.log("Welcome");
+// }
+
+// function extraWork() {
+//   console.log("extraWork");
+// }
+
+// greet();
+// welcom();
+// extraWork();
+
+
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+
+// setTimeout(()=>{
+//     console.log("Hello");
+// },2000)
+
+// function world() {
+//   console.log("world");
+// }
+
+// function remainingWork() {
+//   console.log("remaining Work");
+// }
+
+
+// world();
+// remainingWork();
+
+
+
+// 1. Registration
+
+// 2. welcome mail
+
+// 3. saving user data
+
+// 4. sending sucessful mail
